033fa64b48d4be6454ec386581b3bbd0c213e2bb
3rooks <Brookslynx@gmail.com>
2023-05-24 10:54:46 -0300

    🔧 jest config

--- src/error/error.js
+++ src/error/error.js
@@ -2,7 +2,7 @@ import { COLORS } from '#constants/colors.js';
 
 export class Exception extends Error {
     static catch(error) {
-        console.log('\n' + COLORS.RED, error.message);
+        console.log('\n\n' + COLORS.RED, error.message);
         console.log(COLORS.RED, error.stack);
         process.exit(1);
     }
